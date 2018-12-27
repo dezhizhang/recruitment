f5ab0d45f125e0e6c49a9bfc15addf94eb67cca7
dezhizhang <1541609448@qq.com>
2018-12-27 18:42:14 +0800

    连接express

--- server/server.js
+++ server/server.js
@@ -8,8 +8,13 @@ app.get('/',(req,res) => {
 
 })
 
+app.get('/api/data',(req,res) => {
+    res.json({'name':'zhang','value':'123'});
+    
+})
+
 app.listen('8082','localhost',() => {
     console.log('node在运行中');
-    
+
 });
 
