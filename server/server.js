0776fccbd2e7e39e64cc9a82399ef110399b9dfc
dezhizhang <1541609448@qq.com>
2018-12-27 20:00:14 +0800

    添加数据库连接

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+let url = 'mongodb://localhost:27017';
+
+mongoose.connect(url);
+mongoose.connection.on('connected',() => {
+    console.log('数据库连接成功');
+
+})
+
 
 //新建app
 let app = express();
@@ -10,7 +20,7 @@ app.get('/',(req,res) => {
 
 app.get('/api/data',(req,res) => {
     res.json({'name':'zhang','value':'123'});
-    
+
 })
 
 app.listen('8082','localhost',() => {
