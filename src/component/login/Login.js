c940325328f5db7a07c970c1b57ba0332e7fb2ae
dezhizhang <1541609448@qq.com>
2019-01-03 22:42:27 +0800

    封装目录结构

---
+++ src/component/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Login extends React.Component{
+    render(){
+        return (<div>
+            LOGIN
+        </div>)
+    }
+}
+
+export default Login;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
+import reducers from './reducer'
+import Login from './component/login/Login';
+import Register from './component/register/Register';
+import './config'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const store = createStore(reducers, compose(
+	applyMiddleware(thunk)
+))
+
+ReactDom.render(
+	(<Provider store={store}>
+		<BrowserRouter>
+			<Switch>
+                <Route path='/login' exact component={Login}></Route>
+                <Route path='/register' exact component={Register}></Route>
+            </Switch>
+		</BrowserRouter>
+	</Provider>),
+	document.getElementById('root')
+)
