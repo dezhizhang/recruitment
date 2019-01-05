c6bed55b31b5e3591ae5f960d7bc7904718c1bc7
dezhizhang <1541609448@qq.com>
2019-01-05 19:12:55 +0800

    添加redux

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,9 @@
 
 // 合并所有reducer 并且返回
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import { user } from './redux/user';
 
 
-export default combineReducers({})
+
+export default combineReducers({ user })
 
